eb2a5c4a0574bc614f5666bebd3f9365d6697704
Drake Gooding <dgooding1008@gmail.com>
2021-08-23 18:03:24 -0500

    Routes are now working on front end

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+
 const PORT = process.env.PORT || 3000
 
 const app = express();
@@ -10,13 +11,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-// routes
+//routes
 app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
